e2d4fc068acc6e415efdd19922583d4e40c24c88
Arash Alikhani <arashalikhani@MacBook-Pro.local>
2019-05-05 17:17:49 -0700

    finish Rec view tu

--- app/src/main/java/com/example/myapplication/mvx/Recycle/RecAdapter.java
+++ app/src/main/java/com/example/myapplication/mvx/Recycle/RecAdapter.java
@@ -9,21 +9,59 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.myapplication.mvx.R;
+import com.example.myapplication.mvx.mvvmRoom.NoteAdapter;
 
 import java.util.ArrayList;
 
 public class RecAdapter extends RecyclerView.Adapter<RecAdapter.RecViewHolder> {
     ArrayList<RecItem> recItems;
+    private OnItemClickListener listener ;
+
+     public interface  OnItemClickListener {
+         void onItemClick(int pos);
+         void onDeleteClick(int pos);
+     }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener  ){
+         this.listener = onItemClickListener;
+    }
     public static class RecViewHolder extends RecyclerView.ViewHolder{
         public ImageView iv;
         public TextView tv_up;
         public TextView tv_down;
+        public  ImageView iv_delete;
 
-        public RecViewHolder(@NonNull View itemView) {
+        public RecViewHolder(@NonNull View itemView , final OnItemClickListener listener) {
             super(itemView);
             iv = itemView.findViewById(R.id.iv);
             tv_up =  itemView.findViewById(R.id.tv_up);
             tv_down =  itemView.findViewById(R.id.tv_down);
+            iv_delete = itemView.findViewById(R.id.iv_delete);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener != null){
+                        int pos = getAdapterPosition();
+                        if(pos!=RecyclerView.NO_POSITION){
+                            listener.onItemClick(pos);
+                        }
+                    }
+
+                }
+            });
+
+            iv_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener != null){
+                        int pos = getAdapterPosition();
+                        if(pos!=RecyclerView.NO_POSITION){
+                            listener.onDeleteClick(pos);
+                        }
+                    }
+
+                }
+            });
         }
     }
 
@@ -35,7 +73,7 @@ public class RecAdapter extends RecyclerView.Adapter<RecAdapter.RecViewHolder> {
     @Override
     public RecViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rec_item, parent, false);
-        RecViewHolder recViewHolder =  new RecViewHolder(v);
+        RecViewHolder recViewHolder =  new RecViewHolder(v,listener);
         return recViewHolder ;
     }
 
@@ -51,4 +89,6 @@ public class RecAdapter extends RecyclerView.Adapter<RecAdapter.RecViewHolder> {
     public int getItemCount() {//define how many items we want to be in our list
         return recItems.size();
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/mvx/Recycle/RecItem.java
+++ app/src/main/java/com/example/myapplication/mvx/Recycle/RecItem.java
@@ -22,4 +22,7 @@ public class RecItem {
     public String getmText2() {
         return mText2;
     }
+    public void changeText1(String text){
+        mText1 = text;
+    }
 }
